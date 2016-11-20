ec0e336385076da5361970ee603f568d71b05f24
Jakub Wida <wida_1113470@spk-ssh-access1.if.uj.edu.pl>
2016-11-20 12:28:42 +0100

    first commit

---
+++ projekt_2/projekt_2_script.js
@@ -0,0 +1,28 @@
+
+
+function button_1()
+	{
+	window.alert("Oto Alert");
+	console.log("hello");
+	}
+
+function button_red()
+	{
+	document.getElementById("przyklad_2").style.backgroundColor="red";
+	
+	}
+function button_green()
+	{
+document.getElementById("przyklad_2").style.backgroundColor="green";
+	}
+function button_blue()
+	{
+document.getElementById("przyklad_2").style.backgroundColor="blue";
+	}
+
+
+$(document).ready(function() 
+    { 
+        $("#myTable").tablesorter(); 
+    } 
+); 
